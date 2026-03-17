0731ffc08ee5b0a39838d84a6f09dbef355d3857
sayhussain <s.3bdulla17@gmail.com>
2026-03-17 23:49:36 +0300

    comments

--- js/login.js
+++ js/login.js
@@ -2,10 +2,11 @@ const form = document.getElementById("loginForm")
 
 form.addEventListener("submit", function(event){
     event.preventDefault()
+    //read username and password from form
     const username = document.getElementById(username).value
     const password = document.getElementById(password).value
     
-
+    //change username and pass to base64 for basic auth
     const base64Credentials = btoa(username + ":" + password)   
 
     fetch("https://((DOMAIN))/api/auth/signin", {
@@ -15,6 +16,7 @@ form.addEventListener("submit", function(event){
             "Content-Type": "Application/json"
         }
     })
+    //request to backend and handle response
     .then(response => {
         if(!response.ok){
             document.getElementById("errorMessage").innerText = "Invalid username or password"
@@ -23,6 +25,7 @@ form.addEventListener("submit", function(event){
         }
         return response.json()
     })
+    //if login successful, store token and redirect to dashboard
     .then(data => {
         const token = data.token
         localStorage.setItem("token", token)
